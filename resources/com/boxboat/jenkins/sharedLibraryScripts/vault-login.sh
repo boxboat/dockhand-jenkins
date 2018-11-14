929cd5c8c8be364858c0bfeb2e21971fdee6c883
Matthew DeVenny <matt@boxboat.com>
2018-11-14 12:52:19 -0500

    Add vault-login.sh

---
+++ resources/com/boxboat/jenkins/sharedLibraryScripts/vault-login.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+usage () {
+    echo "Usage: VAULT_ROLE_ID=<vault-role-id> VAULT_SECRET_ID=<vault-secret-id> source vault-login.sh" >&2
+}
+
+help=0
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "-h" | "--help")
+            help=1
+            ;;
+    esac
+    shift
+done
+
+if [ $help -eq 1 ]; then
+    usage
+    return
+fi
+
+VAULT_AUTH_METHOD="ROLE"
+
+if [[ "${VAULT_ROLE_ID}" = "" || "${VAULT_SECRET_ID}" = "" ]]; then
+  VAULT_AUTH_METHOD="TOKEN"
+fi
+
+if [[ "${VAULT_AUTH_METHOD}" = "TOKEN" && "${VAULT_TOKEN}" = "" ]]; then
+  usage
+  return
+fi
+
+if [ "${VAULT_AUTH_METHOD}" = "ROLE" ]; then
+export VAULT_TOKEN=$(vault write --format=json auth/approle/login \
+    role_id=${VAULT_ROLE_ID} \
+    secret_id=${VAULT_SECRET_ID} | jq -r .auth.client_token)
+fi
+
+set +e
