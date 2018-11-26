6ffa448192fa1cdf17adc7ea3d0f2baf39cdfb5c
Matthew DeVenny <matt@boxboat.com>
2018-11-26 12:29:10 -0500

    Use fileExists

--- src/com/boxboat/jenkins/library/LibraryScript.groovy
+++ src/com/boxboat/jenkins/library/LibraryScript.groovy
@@ -2,15 +2,12 @@ package com.boxboat.jenkins.library
 
 class LibraryScript {
 
-    static Map<String, Boolean> loaded = [:]
-
     static String run(steps, String script) {
         def path = "sharedLibraryScripts/${script}"
-        if (!loaded.containsKey(script)) {
+        if (!steps.fileExists(path)) {
             String data = steps.libraryResource(resource: "com/boxboat/jenkins/${path}", encoding: "Base64")
             steps.writeFile(file: path, text: data, encoding: "Base64")
             steps.sh "chmod +x ${path}"
-            loaded[script] = true
         }
         return "./${path}"
     }
