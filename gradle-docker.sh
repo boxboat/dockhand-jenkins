9f15c5bb65c759256e904f821d5bf28702094a54
Caleb Lloyd <caleblloyd@gmail.com>
2020-03-21 08:06:26 +0300

    add github actions
    Signed-off-by: Caleb Lloyd <caleblloyd@gmail.com>

--- gradle-docker.sh
+++ gradle-docker.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
 cd $(dirname $0)
 
-docker run --rm \
+if ! docker volume inspect gradle-cache >/dev/null 2>&1; then
+    docker volume create gradle-cache
+fi
+
+docker run --rm -i \
     -v "gradle-cache:/home/gradle/.gradle" \
     -v "$(pwd):/home/gradle/project" \
     -w "/home/gradle/project" \
