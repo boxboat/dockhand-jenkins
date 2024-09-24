8e4686b147aa4a206a90ef37086790064e1e72e4
Matthew DeVenny <matt@boxboat.com>
2024-09-24 09:29:36 -0700

    Adjust test to allow for local execution on macOS
    Signed-off-by: Matthew DeVenny <matt@boxboat.com>

--- test/com/boxboat/jenkins/test/pipeline/deploy/kubernetes/KubePodTest.groovy
+++ test/com/boxboat/jenkins/test/pipeline/deploy/kubernetes/KubePodTest.groovy
@@ -3,24 +3,21 @@ package com.boxboat.jenkins.test.pipeline.deploy.kubernetes
 import com.boxboat.jenkins.pipeline.deploy.kubernetes.KubePod
 import org.junit.Test
 
-import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertTrue
 
 class KubePodTest {
 
     @Test
     void testKubeLogs() {
-        def kubeLogs = KubePod.pollScript(outFile: "out.log", namespace: "test-ns", container: "nginx", labels: "a=b,c=d")
-        assertEquals("""
-            ${System.getProperty('java.io.tmpdir')}/sharedLibraryScripts/pod-logs.sh -o "out.log" -n "test-ns" -l "a=b,c=d" -c "nginx"
-        """.trim(), kubeLogs.trim())
+        def kubeLogs = KubePod.pollScript(outFile: "out.log", namespace: "test-ns", container: "nginx", labels: "a=b,c=d").trim()
+        assertTrue(kubeLogs.trim().endsWith("""/sharedLibraryScripts/pod-logs.sh -o "out.log" -n "test-ns" -l "a=b,c=d" -c "nginx" """.trim()))
+
     }
 
     @Test
     void testKubeExec() {
-        def kubeExec = KubePod.execScript(namespace: "test-ns", labels: "a=b,c=d", container: "nginx", command: ["cat", "test.yaml"])
-        assertEquals("""
-            ${System.getProperty('java.io.tmpdir')}/sharedLibraryScripts/pod-exec.sh -n "test-ns" -l "a=b,c=d" -c "nginx" "cat" "test.yaml"
-        """.trim(), kubeExec.trim())
+        def kubeExec = KubePod.execScript(namespace: "test-ns", labels: "a=b,c=d", container: "nginx", command: ["cat", "test.yaml"]).trim()
+        assertTrue(kubeExec.endsWith("""/sharedLibraryScripts/pod-exec.sh -n "test-ns" -l "a=b,c=d" -c "nginx" "cat" "test.yaml" """.trim()))
     }
 
 }
